e9c7c385c2d09b18cc0f69f176a98d3d73091ad5
LuisDLF <60014398+LuisDLF@users.noreply.github.com>
2020-03-26 16:27:15 -0600

    Update configAPP.php

--- core/configAPP.php
+++ core/configAPP.php
@@ -1,8 +1,8 @@
 <?php
-const SERVER="localhost";
-const DB="negocios";
-const USER="root";
-const PASS="";
+const SERVER="127.0.0.1:3306";
+const DB="u654513175_Enpotrerochico";
+const USER="u654513175_daniel";
+const PASS="12345";
 const SGBD='mysql:host='.SERVER.';dbname='.DB;
 const METHOD='AES-256-CBC';
 const SECRET_KEY='$DANIEL@2020';
