2e25efd5d0c15cd3d61a8327b7109e1a2b7f418c
LuisDLF <60014398+LuisDLF@users.noreply.github.com>
2020-03-26 06:09:26 -0600

    Update Hoteles-view.php

--- vistas/contenidos/Hoteles-view.php
+++ vistas/contenidos/Hoteles-view.php
@@ -12,22 +12,21 @@ $resultado=$hoteles->Obtain_Hotels_Controller();
 
 ?>
 
-<div class="container no-gutters">
-<h3><?php echo Hoteles_titulo; ?></h3>
-      <div class="row no-gutters">
+<div class="container m-2 text-center item-center">
+      <div class="row">
 
       <?php    
       while ($rows = mysqli_fetch_array($resultado)){ 
       ?>
   
 
-          <div class="col-md-8 no-gutters">
+          <div class="col-md-8">
          <div class="card- mb-2 bg-dark text-white rounded">
             <?php
             echo '<img  class="card-img" style="filter: brightness(50%); height:200px" src="data:image/jpeg;base64,'.base64_encode( $rows[2] ).'" alt="Card image">'
             ?>
             <div class="card-img-overlay">
-               <h5 class="card-title"><?php echo utf8_encode($rows[0]);?></h5>
+               <h5 class="card-title text-light"><?php echo utf8_encode($rows[0]);?></h5>
                <p class="card-text"><?php echo utf8_encode($rows[1])?></p>
                <a class="btn btn-primary btn-sm" role="button" href="http://www.jquery2dotnet.com/">Continue reading</a>
             </div>
