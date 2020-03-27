724b7af6a1d0045e15b4ad1b7ccebb1c1fa29111
LuisDLF <60014398+LuisDLF@users.noreply.github.com>
2020-03-26 21:10:13 -0600

    Update Hoteles-view.php

--- vistas/contenidos/Hoteles-view.php
+++ vistas/contenidos/Hoteles-view.php
@@ -28,7 +28,7 @@ $resultado=$hoteles->Obtain_Hotels_Controller();
             <div class="card-img-overlay">
                <h5 class="card-title text-light"><?php echo utf8_encode($rows[1]);?></h5>
                <p class="card-text"><?php echo utf8_encode($rows[2])?></p>
-               <a class="btn btn-primary btn-sm" role="button" href=<?php echo "'".SERVERURL."Hotel"."?Id_Hotel=".echo utf8_encode($rows[1]);?>."'"?>>Continue reading</a>
+               <a class="btn btn-primary btn-sm" role="button" href=<?php echo "'".SERVERURL."Hotel"."?Id_Hotel=".echo utf8_encode($rows[0]);?>."'"?>>Continue reading</a>
             </div>
          </div>
       </div>
