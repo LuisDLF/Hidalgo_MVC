60c7507df4d2da0777b1a597087f237359061e1b
LuisDLF <60014398+LuisDLF@users.noreply.github.com>
2020-03-26 21:47:16 -0600

    Update Hotel-view.php

--- vistas/contenidos/Hotel-view.php
+++ vistas/contenidos/Hotel-view.php
@@ -30,7 +30,7 @@ $resultado=$hoteles->Obtain_Hotel_Controller($_GET["Id_Hotel"]);
      
 
        <?php
-            echo '<img class="img-fluid" src="data:image/jpeg;base64,'.base64_encode( $resultado[3] ).'" alt="Card image" style="height:750px; width:500px;">';
+            echo '<img class="img-fluid" src="data:image/jpeg;base64,'.base64_encode( $resultado[3] ).'" alt="Card image">';
             ?>
     </div>
 
