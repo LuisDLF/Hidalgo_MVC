a024a033aef0dbbbae2f725e752952d0ff4dc3c2
LuisDLF <60014398+LuisDLF@users.noreply.github.com>
2020-03-26 21:30:47 -0600

    Update Hotel-view.php

--- vistas/contenidos/Hotel-view.php
+++ vistas/contenidos/Hotel-view.php
@@ -21,9 +21,7 @@ $resultado=$hoteles->Obtain_Hotel_Controller($_GET["Id_Hotel"]);
 <div class="container">
 
   <!-- Portfolio Item Heading -->
-  <h1 class="my-4">Page Heading
-    <small>Secondary Text</small>
-  </h1>
+  <h1 class="my-4"><?php echo $resultado[1]; ?></h1>
 
   <!-- Portfolio Item Row -->
   <div class="row">
@@ -33,8 +31,8 @@ $resultado=$hoteles->Obtain_Hotel_Controller($_GET["Id_Hotel"]);
     </div>
 
     <div class="col-md-4">
-      <h3 class="my-3"><<?php echo $resultado[0]; ?></h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+      <h3 class="my-3"></h3>
+      <p><?php echo $resultado[2]; ?></p>
       <h3 class="my-3">Project Details</h3>
       <ul>
         <li>Lorem Ipsum</li>
