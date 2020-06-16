b27577d7c2bdf96acc2ed06f1e2ed3581f08d9a0
fbayala <60682615+fbayala@users.noreply.github.com>
2020-06-16 00:08:27 -0500

    INDEX BANER Y CONTACTO
    Cambio de fotogracias del baner se agregron 4 fotografias nuevas con diferentes medidas.
    
    Acerca de nosotros se agrego una breve descripcion y 2 fotografias.
    
    Video: se agrego un video de mi canal

--- vistas/contenidos/Index-view.php
+++ vistas/contenidos/Index-view.php
@@ -5,44 +5,44 @@
                     <div class="row">
                         <div class="col-xl-12">
                             <div class="slider_text text-center">
-                                <h3>Montana Resort</h3>
-                                <p>Unlock to enjoy the view of Martine</p>
+                                <h3>EN POTRERO CHICO</h3>
+                                <p>Tu mejor Guia Local</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="single_slider  d-flex align-items-center justify-content-center" style="background-image: url(./vistas/assets/banner/banner.png);">
+            <div class="single_slider  d-flex align-items-center justify-content-center" style="background-image: url(./vistas/assets/banner/banner2.png);">
                 <div class="container">
                     <div class="row">
                         <div class="col-xl-12">
                             <div class="slider_text text-center">
                                 <h3>EN POTRERO CHICO</h3>
-                                <p>Unlock to enjoy the view of Martine</p>
+                                <p>Tu mejor Guia Local</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="single_slider d-flex align-items-center justify-content-center" style="background-image: url(./vistas/assets/banner/banner.png);">
+            <div class="single_slider d-flex align-items-center justify-content-center" style="background-image: url(./vistas/assets/banner/banneer3.png);">
                 <div class="container">
                     <div class="row">
                         <div class="col-xl-12">
                             <div class="slider_text text-center">
-                                <h3>Montana Resort</h3>
-                                <p>Unlock to enjoy the view of Martine</p>
+                                <h3>EN POTRERO CHICO</h3>
+                                <p>Tu mejor Guia Local</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="single_slider  d-flex align-items-center justify-content-center" style="background-image: url(./vistas/assets/banner/banner.png);">
+            <div class="single_slider  d-flex align-items-center justify-content-center" style="background-image: url(./vistas/assets/banner/banner4.png);">
                 <div class="container">
                     <div class="row">
                         <div class="col-xl-12">
                             <div class="slider_text text-center">
-                                <h3>Life is Beautiful</h3>
-                                <p>Unlock to enjoy the view of Martine</p>
+                                <h3>EN POTRERO CHICO</h3>
+                                <p>Tu mejor Guia Local</p>
                             </div>
                         </div>
                     </div>
@@ -60,22 +60,22 @@
                     <div class="about_info">
                         <div class="section_title mb-20px">
                             <span>About Us</span>
-                            <h3>A Luxuries Hotel <br>
-                                with Nature</h3>
+                            <h3>En Potrero Chico <br>
+                            Tu mejor Guia Local</h3>
                         </div>
-                        <p>Suscipit libero pretium nullam potenti. Interdum, blandit phasellus consectetuer dolor ornare
-                            dapibus enim ut tincidunt rhoncus tellus sollicitudin pede nam maecenas, dolor sem. Neque
-                            sollicitudin enim. Dapibus lorem feugiat facilisi faucibus et. Rhoncus.</p>
+                        <p>Somos una Empresa en Publicidad radical del Municipio de Hidalgo Nuevo León interesada en expander el reconocimiento a través de nuestro sitio web a nivel Mundial.
+                            Buscamos negocios interesados en promocionarse desde nuestro sitio web a un costo Accesible, 
+                            CONTÁCTANOS y conocé las ventajas de nuestro servicio.</p>
                         <a href="#" class="line-button">Learn More</a>
                     </div>
                 </div>
                 <div class="col-xl-7 col-lg-7">
                     <div class="about_thumb d-flex">
                         <div class="img_1">
-                            <img src="vistas/assets/about/about_1.png" alt="">
+                            <img src="vistas/assets/about/about1.png" alt="">
                         </div>
                         <div class="img_2">
-                            <img src="vistas/assets/about/about_2.png" alt="">
+                            <img src="vistas/assets/about/about2.png" alt="">
                         </div>
                     </div>
                 </div>
@@ -150,9 +150,9 @@
     <div class="video_area video_bg overlay">
         <div class="video_area_inner text-center">
             <span>Montana Sea View</span>
-            <h3>Relax and Enjoy your <br>
-                Vacation </h3>
-            <a href="https://www.youtube.com/watch?v=vLnPwxZdW4Y" class="video_btn popup-video">
+            <h3>Relájate y disfruta tus <br>
+                Vacaciones </h3>
+            <a href="https://www.youtube.com/watch?v=8En5Hsmgk0Q" class="video_btn popup-video">
                 <i class="fa fa-play"></i>
             </a>
         </div>
