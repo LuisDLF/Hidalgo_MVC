433818b8a109583013735934428ca6d44bdc18c9
LuisDLF <60014398+LuisDLF@users.noreply.github.com>
2020-06-28 15:30:57 -0500

    Update Hoteles-view.php

--- vistas/contenidos/Hoteles-view.php
+++ vistas/contenidos/Hoteles-view.php
@@ -54,6 +54,36 @@ $resultado=$hoteles->Obtain_Hotels_Controller();
                         <a  class="book_now" href=<?php echo "'".SERVERURL."Hotel"."?Id_Hotel=".utf8_encode($rows[0])."'"?>>book now</a>
                     </div>
                 </div>
+                <div class=" card col-xl-4 col-md-4">
+                    <div class="card-body single_offers">
+                        <div class="about_thumb">
+                            <img src="vistas/assets/offers/1a.png" alt="">
+                        </div>
+                        <h3><?php echo utf8_encode($rows[1]);?><br>
+                            </h3>
+                        <ul>
+                            <li>.</li>
+                            <li>.</li>
+                            <li>.</li>
+                        </ul>
+                        <a  class="book_now" href=<?php echo "'".SERVERURL."Hotel"."?Id_Hotel=".utf8_encode($rows[0])."'"?>>book now</a>
+                    </div>
+                </div>
+                <div class=" card col-xl-4 col-md-4">
+                    <div class="card-body single_offers">
+                        <div class="about_thumb">
+                            <img src="vistas/assets/offers/1a.png" alt="">
+                        </div>
+                        <h3><?php echo utf8_encode($rows[1]);?><br>
+                            </h3>
+                        <ul>
+                            <li>.</li>
+                            <li>.</li>
+                            <li>.</li>
+                        </ul>
+                        <a  class="book_now" href=<?php echo "'".SERVERURL."Hotel"."?Id_Hotel=".utf8_encode($rows[0])."'"?>>book now</a>
+                    </div>
+                </div>
               <?php } ?>
               
             </div>
