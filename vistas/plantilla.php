f407187c6c81a831a93ef6a5fecdc5f4ef4166d6
LuisDLF <60014398+LuisDLF@users.noreply.github.com>
2020-10-05 21:12:00 -0500

    Update plantilla.php

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -7,6 +7,7 @@
 	  <meta name="description" content="Sitio Web de Hidalgo Nuevo Leon">
   <meta name="keywords" content="Hotel,Hidalgo,PotreroChico,Potrero,escalar,Nuevo Leon,Potrero Chico">
   <meta name="author" content="Luis Daniel">
+
 <link rel="stylesheet" href="<?php echo SERVERURL; ?>vistas/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo SERVERURL; ?>vistas/css/owl.carousel.min.css">
     <link rel="stylesheet" href="<?php echo SERVERURL; ?>vistas/css/magnific-popup.css">
@@ -101,30 +102,9 @@
                                 enpotrerochico@gmail.com</p>
                         </div>
                     </div>
-                    <div class="col-xl-2 col-md-6 col-lg-2">
-                        <div class="footer_widget">
-                            <h3 class="footer_title">
-                                Navigation
-                            </h3>
-                            <ul>
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">Rooms</a></li>
-                                <li><a href="#">About</a></li>
-                                <li><a href="#">News</a></li>
-                            </ul>
-                        </div>
-                    </div>
+                  
                     <div class="col-xl-4 col-md-6 col-lg-4">
-                        <div class="footer_widget">
-                            <h3 class="footer_title">
-                                Newsletter
-                            </h3>
-                            <form action="#" class="newsletter_form">
-                                <input type="text" placeholder="Enter your mail">
-                                <button type="submit">Sign Up</button>
-                            </form>
-                            <p class="newsletter_text">Subscribe newsletter to get updates</p>
-                        </div>
+                      
                     </div>
                 </div>
             </div>
@@ -135,9 +115,6 @@
                 <div class="row">
                     <div class="col-xl-8 col-md-7 col-lg-9">
                         <p class="copy_right">
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     </div>
                     <div class="col-xl-4 col-md-5 col-lg-3">
                         <div class="socail_links">
