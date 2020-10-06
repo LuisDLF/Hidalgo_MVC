6b7cec7b55f955e7d23312b20cd227ce0f81fc95
LuisDLF <60014398+LuisDLF@users.noreply.github.com>
2020-10-05 21:06:04 -0500

    Update Hotel-view.php

--- vistas/contenidos/Hotel-view.php
+++ vistas/contenidos/Hotel-view.php
@@ -15,57 +15,11 @@ $resultado=$hoteles->Obtain_Hotel_Controller($_GET["Id_Hotel"]);
 
 ?>
 
-
- 
-
 <section class="container" style="margin-top: 200px;">
-  <div id="carouselExampleIndicators" class="carousel slide mt-5" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
+ 
+ 
     <div class="carousel-item active">
        <?php echo '<img class="img-fluid" src="data:image/jpeg;base64,'.base64_encode( $resultado[3] ).'" alt="Card image">';?>
     </div>
-    <div class="carousel-item">
-      <?php echo '<img class="img-fluid" src="data:image/jpeg;base64,'.base64_encode( $resultado[3] ).'" alt="Card image" >';?>
-    </div>
-    <div class="carousel-item">
-       <?php echo '<img class="img-fluid" src="data:image/jpeg;base64,'.base64_encode( $resultado[3] ).'" alt="Card image" >';?>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+   
 </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
